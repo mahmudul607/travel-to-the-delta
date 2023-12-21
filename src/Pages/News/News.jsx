8b66d5cbd7e041503bc3aed03a918fd6ee8ef1b0
unknown <mahmudul.cse.eng@gmail.com>
2023-12-21 23:31:10 +0600

    updated

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,6 +1,7 @@
 import { Link} from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import './News.css'
+import NewsCarousel from "./NewsCarousel";
 const News = () => {
    
 
@@ -10,12 +11,13 @@ const News = () => {
             <div className="backdrop-saturate-150 bg-black/30 h-full">
                 <Navbar></Navbar>
                 <div className=" w-full text-left pl-6  grid md:grid-cols-6 ">
-                    <div className="text-white md:col-span-2 h-full relative top-48 ">
+                    <div className="text-white md:col-span-2 h-full relative top-32 ">
                         <h1 className="text-4xl uppercase">{city_name} BAZAR</h1>
                         <p>{city_name} Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
                        <Link to='/booking' className="btn btn-lg bg-yellow-600 mt-4">Booking</Link>
                     </div>
-                    <div className="md:col-span-4">
+                    <div className="md:col-span-4 h-full relative top-36 overflow-hidden">
+                     <NewsCarousel></NewsCarousel>
 
                     </div>
                    
